a34389f9b9044b348725fb859512ae9bdf00d680
malikovboo <malikovboo@gmail.com>
2023-10-16 01:19:55 +0500

    added color viewer and brush charger

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ import mediapipe as mp
 
 
 class DragAndDropCircle:
-    def __init__(self, px, py, radius=50, color_index=0):
+    def __init__(self, px, py, radius=50, color_index=0, brush_thickness=10):
         self.radius = radius
         self.color_index = color_index
         self.px = px
         self.py = py
+        self.brush_thickness = brush_thickness  # новая переменная для толщины кисточки
         self.colors = [(255, 255, 255), (0, 0, 255), (0, 255, 0), (255, 0, 0), (255, 255, 0), (255, 0, 255),
                        (0, 255, 255), (0, 0, 0)]
 
@@ -21,6 +22,9 @@ class DragAndDropCircle:
         return self.colors[self.color_index % len(self.colors)]
 
 
+colors = [(255, 255, 255), (0, 0, 255), (0, 255, 0), (255, 0, 0), (255, 255, 0), (255, 0, 255),
+                       (0, 255, 255), (0, 0, 0)]
+color_changed = False
 cap = cv2.VideoCapture(0)
 cap.set(3, 1280)
 cap.set(4, 720)
@@ -64,7 +68,7 @@ while True:
         y5 = myHand.landmark[12].y * h
         y6 = y2
 
-        length = math.hypot(abs(x1 - x2), abs(y1 - y2))
+        length = math.hypot(abs(x1 - x5), abs(y1 - y5))
         length1 = math.hypot(abs(x3 - x4), abs(y3 - y4))
         length2 = math.hypot(abs(x5 - x6), abs(y5 - y6))
 
@@ -76,7 +80,24 @@ while True:
             circles = []
 
         elif length2 < 40:
-            color_index += 1
+            if not color_changed:
+                color_index += 1
+                color_changed = True
+        else:
+            color_changed = False
+
+    if circles:
+        distance_between_fingers = math.hypot(x1 - x2, y1 - y2)
+        circles[-1].brush_thickness = int(distance_between_fingers / 2)
+
+    for circle in circles:
+        cv2.circle(img, (int(circle.px), int(circle.py)), circle.radius, circle.get_color(),
+                   circle.brush_thickness)  # используйте brush_thickness
+
+        # Отобразите текущий выбранный цвет в углу экрана
+    current_color = colors[color_index % len(colors)]
+    cv2.rectangle(img, (10, 10), (60, 60), current_color, -1)
+    cv2.putText(img, "Current Color", (80, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
 
     for circle in circles:
         cv2.circle(img, (int(circle.px), int(circle.py)), circle.radius, circle.get_color(), cv2.FILLED)
